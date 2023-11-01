fd2226e16d976c3b801ff012699d2ba8a2d6db51
Tom Aylott <oblivious@subtlegradient.com>
2023-11-01 17:17:31 -0400

    pass the proof to the RN app

--- zupass-verify-server/app/zupass-verify/page.tsx
+++ zupass-verify-server/app/zupass-verify/page.tsx
@@ -6,7 +6,9 @@ export default async function Page({ searchParams: { final_redirect_uri, proof, 
   const isVerified = finished === "true" && proof && (await verifyProof(proof))
 
   if (isVerified && final_redirect_uri) {
-    redirect(final_redirect_uri)
+    const url = new URL(final_redirect_uri)
+    url.searchParams.set("proof", proof)
+    redirect(url.href)
   }
 
   return <div>ERROR</div>
